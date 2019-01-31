350a4455c2367821063521f2f74a6cd82626e730
NicolaosJM <nicmay@gmail.com>
2019-01-31 16:06:49 -0600

    commited most files, testing

---
+++ controllers/food_controller.js
@@ -0,0 +1,31 @@
+var express = require("express");
+
+var router = express.Router();
+
+var food = require("../models/food.js");
+
+router.get("/", function (request, response) {
+    food.all(function (data) {
+        console.log(data);
+        response.render("index", {foodtable: data});
+    });
+});
+
+router.put("/api/update/:id", function (request, response) {
+    food.update(request.params.id, function(result) {
+        response.render("index", {foodtable: result});
+    });
+});
+
+router.post("/api/create", function (request, response) {
+    food.create([
+        "meal_name",
+        "calorie_count"
+    ], [
+        request.body.meal_name
+    ], function (result) {
+        response.json({ id: result.insertId});
+    });
+});
+
+module.exports = router;
