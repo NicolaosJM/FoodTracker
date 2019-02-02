f35b92d2d30dd2c66561f015d97315c209656b8e
NicolaosJM <nicmay@gmail.com>
2019-02-02 10:09:27 -0600

    fixing heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,28 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "food_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "food_db"
+// });
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'food_db'
+    })
+};
+
+var mysql = require('mysql');
+
 
 connection.connect(function (error) {
     if (error) {
